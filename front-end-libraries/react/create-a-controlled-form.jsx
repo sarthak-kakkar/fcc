aaf3007c932c10401292c37316b456bdb1ed7f56
Sarthak Kakkar <sarthak.kakkar@pm.me>
2020-09-24 23:24:27 +0530

    fcc-29

--- front-end-libraries/react/create-a-controlled-form.jsx
+++ front-end-libraries/react/create-a-controlled-form.jsx
@@ -37,3 +37,25 @@ class MyForm extends React.Component {
     );
   }
 }
+
+{/*
+https://forum.freecodecamp.org/t/doubt-this-setstate/342916
+If you want to return an object using an arrow function you have two choices:
+
+Explicit return:
+
+const func = param => {
+  return {
+    prop1: 'value 1',
+    prop2: 'value 2'
+  };
+};
+OR
+Implicit return:
+
+const func = param => ({
+  prop1: 'value 1',
+  prop2: 'value 2'
+});
+Note in the second example, the object returned is surrounded by parentheses. This tells JavaScript to return the value inside.
+*/}
