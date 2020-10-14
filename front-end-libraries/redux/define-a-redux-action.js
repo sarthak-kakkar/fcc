5b19d09461298c862bb65a6bb198b7e137098717
Sarthak Kakkar <sarthak.kakkar@pm.me>
2020-10-14 17:31:49 +0530

    fcc-32

---
+++ front-end-libraries/redux/define-a-redux-action.js
@@ -0,0 +1,9 @@
+// Define an action here:
+const action = {
+    type: 'LOGIN'
+};
+
+/*
+Think of Redux actions as messengers that deliver information about events happening in your app to the Redux store. 
+The store then conducts the business of updating state based on the action that occurred.
+*/
---
+++ front-end-libraries/redux/define-an-action-creator.js
@@ -0,0 +1,19 @@
+const action = {
+  type: 'LOGIN'
+}
+// Define an action creator here:
+/*function actionCreator() {
+  return action;
+}*/
+
+/*const actionCreator = () => {
+  return action
+  };*/
+
+const actionCreator = () => (action);
+
+/*
+After creating an action, the next step is sending the action to the Redux store so it can update its state. 
+In Redux, you define action creators to accomplish this. An action creator is simply a JavaScript function that returns an action. 
+In other words, action creators create objects that represent action events.
+*/
