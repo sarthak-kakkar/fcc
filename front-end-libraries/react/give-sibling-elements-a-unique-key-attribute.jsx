870d4502f20c607b72d993deec93f091a0932ee6
Sarthak Kakkar <sarthak.kakkar@pm.me>
2020-10-14 16:00:21 +0530

    fcc-31

---
+++ front-end-libraries/react/give-sibling-elements-a-unique-key-attribute.jsx
@@ -0,0 +1,22 @@
+const frontEndFrameworks = [
+  'React',
+  'Angular',
+  'Ember',
+  'Knockout',
+  'Backbone',
+  'Vue'
+];
+
+function Frameworks() {
+  const renderFrameworks = frontEndFrameworks.map(element => {
+    let key = element + frontEndFrameworks.indexOf(element);
+    return <li key={key}>{element}</li>});
+  return (
+    <div>
+      <h1>Popular Front End JavaScript Frameworks</h1>
+      <ul>
+        {renderFrameworks}
+      </ul>
+    </div>
+  );
+};
